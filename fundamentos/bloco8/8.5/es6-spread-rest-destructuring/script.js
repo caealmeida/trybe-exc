56011f673831af97d0f002d62e418598d64c66d4
caealmeida <caetanoaraujo_@hotmail.com>
2022-04-13 20:21:48 -0300

    8.5

---
+++ fundamentos/bloco8/8.5/es6-spread-rest-destructuring/script.js
@@ -0,0 +1,147 @@
+// Exc 1 - Dado o código abaixo, complete-o de forma que seja impressa a área dos 3 retângulos. O código deve retornar em sequência 2 , 15 e 54 .
+// -------------------------------------------------------------------------------------------------------------------------------------------------
+
+const rectangleArea = (width, height) => width * height;
+
+const rectangle1 = [1, 2];
+const rectangle2 = [3, 5];
+const rectangle3 = [6, 9];
+const rectangles = [rectangle1, rectangle2, rectangle3];
+
+rectangles.forEach((rectangle) => {
+  rectangleArea(...rectangle);
+
+  //   console.log(rectangle[0] * rectangle[1]);
+});
+
+// Exc 2 -  Crie uma função sum que dado um número variável de elementos retorna a soma desses elementos.
+// ------------------------------------------------------------------------------------------------------------------------------------------------
+
+const numbers = [32, 15, 3, 2, -5, 56, 10]; //113
+
+const sum = (...elementos) => elementos.reduce((acc, num) => acc + num);
+
+// console.log(sum(2, 4, 6, 8, 10));
+// console.log(sum(...numbers));
+
+// Exc 3 - Escreva a função personLikes , que dado um objeto de parâmetro que representa uma pessoa, retorna todos os gostos daquela pessoa.
+// ------------------------------------------------------------------------------------------------------------------------------------------------
+
+const alex = {
+  name: 'Alex',
+  age: 26,
+  likes: ['fly fishing'],
+  nationality: 'Australian',
+};
+
+const gunnar = {
+  name: 'Gunnar',
+  age: 30,
+  likes: ['hiking', 'scuba diving', 'taking pictures'],
+  nationality: 'Icelandic',
+};
+
+// complete a assinatura da função abaixo
+const personLikes = ({ name, age, likes }) => `${name} is ${age} years old and likes ${likes.join(', ')}.`;
+
+// console.log(personLikes(alex));
+// console.log(personLikes(gunnar));
+
+// Exc 4 - Escreva uma função filterPeople que, dada uma lista de pessoas, retorna todas as pessoas australianas que nasceram no século 20:
+// ------------------------------------------------------------------------------------------------------------------------------------------------
+
+const people = [
+  {
+    name: 'Nicole',
+    bornIn: 1992,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Harry',
+    bornIn: 2008,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Toby',
+    bornIn: 1901,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Frida',
+    bornIn: 1960,
+    nationality: 'Dannish',
+  },
+  {
+    name: 'Fernando',
+    bornIn: 2001,
+    nationality: 'Brazilian',
+  },
+];
+
+const filterPeople = (array) =>
+  array.filter(({ bornIn, nationality }) => bornIn >= 1901 && bornIn <= 2000 && nationality === 'Australian');
+
+// console.log(filterPeople(people));
+
+// Exc 5 -  Escreva a função swap , que dado um array de 3 elementos, retorna um novo array com o primeiro e terceiro elementos trocados.
+// Detalhe: você precisa fazer essa função gastando 1 linha só:
+// ------------------------------------------------------------------------------------------------------------------------------------------------
+
+const myList = [1, 2, 3];
+
+const swap = ([x, y, z]) => [z, y, x];
+
+// console.log(myList);
+// console.log(swap(myList));
+
+// Exc 6 - Suponha que você esteja lidando com carros e, da forma como o problema lhe foi entregue, cada carro é modelado como um array.
+// Porém, essa modelagem está baixo nível. Cria uma função toObject que, dada uma lista, retorna um objeto representando o carro:
+// ------------------------------------------------------------------------------------------------------------------------------------------------
+
+const palio = ['Palio', 'Fiat', 2019];
+const shelbyCobra = ['Shelby Cobra', 'Ford', 1963];
+const chiron = ['Chiron', 'Bugatti', 2016];
+
+// escreva toObject abaixo
+const toObject = ([name, brand, year]) => ({ name, brand, year });
+
+// console.log(toObject(palio));
+// console.log(toObject(shelbyCobra));
+// console.log(toObject(chiron));
+
+// Exc 7 - Escreva uma função shipLength que, dado um objeto representando um navio, retorna o comprimento dele, mostrando também a devida unidade de comprimento:
+// ------------------------------------------------------------------------------------------------------------------------------------------------
+
+const ships = [
+  {
+    name: 'Titanic',
+    length: 269.1,
+    measurementUnit: 'meters',
+  },
+  {
+    name: 'Queen Mary 2',
+    length: 1132,
+    measurementUnit: 'feet',
+  },
+  {
+    name: 'Yamato',
+    length: 256,
+    measurementUnit: 'meters',
+  },
+];
+
+// escreva shipLength abaixo
+const shipLength = ({ name, length, measurementUnit }) => `${name} is ${length} ${measurementUnit} long`;
+
+// console.log(shipLength(ships[0]));
+// console.log(shipLength(ships[1]));
+// console.log(shipLength(ships[2]));
+
+// Exc 8 - Escreva uma função greet que, dado o nome de uma pessoa, retorna uma mensagem de cumprimento:
+// ------------------------------------------------------------------------------------------------------------------------------------------------
+
+// const greet = () => todo
+
+console.log(greet('John')); // 'Hi John'
+console.log(greet('John', 'Good morning')); // 'Good morning John'
+console.log(greet('Isabela', 'Oi')); // 'Oi Isabela'
